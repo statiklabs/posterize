4af9378e7fcfa981d22ece61d590d28e5b113897
yansarazin <yansarazin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-01 21:31:44 +0000

    Complete rewrite. New admin panel for settings and is now using Posterous API instead of email.
    git-svn-id: https://svn.wp-plugins.org/posterize/trunk@200844 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- posterize.php
+++ posterize.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Posterize
-Plugin URI: http://statikpulse.com/plugins/posterize
-Description: This plugin will automatically send an email to Posterous that will link to your post. 
-Version: 1.0.1
+Plugin URI: http://statikpulse.com/posterize
+Description: This plugin will automatically cross-post your Wordpress blog entry to your Posterous site. 
+Version: 2.0Beta1
 Author: Yan Sarazin 
 Author URI: http://statikpulse.com
 */
 
-/*  Copyright 2008  Yan Sarazin  (email : yan.sarazin@gmail.com)
+/*  Copyright 2010  Yan Sarazin  (email : yan@statikpulse.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -25,24 +25,104 @@ Author URI: http://statikpulse.com
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+/* When plugin is activated */
+register_activation_hook(__FILE__,'activatePosterize');
+register_deactivation_hook( __FILE__, 'deactivatePosterize' );
+
+function activatePosterize() {
+   add_option("posterous_email", '', '', 'yes');
+   add_option("posterous_password", '', '', 'yes');
+   add_option("post_type", '', '', 'yes');
+}
+
+function deactivatePosterize() {
+   delete_option('posterous_email');
+   delete_option('posterous_password');
+   delete_option('post_type');
+}
+
+if(is_admin()){
+   
+   add_action('admin_menu', 'posterizeAdminMenu');
+   function posterizeAdminMenu(){
+      add_options_page('Posterize Settings', 'Posterize', 'administrator', 'posterize-settings', 'posterizeAdminPage');
+   }
+
+   function posterizeAdminPage(){
+      ?>
+      <form method="post" action="options.php">      
+      <?php wp_nonce_field('update-options'); ?>
+      <h1>Posterize Settings</h1>
+      <table>
+         <tr>
+            <td colspan="2"><h3>Pousterous Login Information</h3></td>
+         </tr>
+         <tr>
+            <td width="100"><label for="posterous_email">Email:</label></td>
+            <td><input name="posterous_email" type="text" id="posterous_email" value="<?php echo get_option('posterous_email'); ?>" /></td>
+         </tr>
+         <tr>
+            <td><label for="posterous_password">Password:</label></td>
+            <td><input name="posterous_password" type="password" id="posterous_password" value="<?php echo get_option('posterous_password'); ?>" /></td>
+         </tr>
+         <tr>
+            <td colspan="2"><h3>What to post</h3></td>
+         </tr>
+         <tr>
+            <td colspan="2"><input type='radio' id="post_type" name="post_type" value="1" <?php if(get_option('post_type')=="1"){ echo "checked='checked'";} ?>> Link back to post</td>
+         </tr>
+         <tr>
+            <td colspan="2"><input type="radio" id="post_type" name="post_type" value="2" <?php if(get_option('post_type')=="2"){ echo "checked='checked'";} ?>> Post full content</td>
+         </tr>
+         <tr>
+            <td colspan="2"><h4>Note: Currently Posterize only supports adding content to your main Posterous site.</h4></td>
+         <tr>
+            <td colspan="2">
+               <input type="hidden" name="action" value="update" />
+               <input type="hidden" name="page_options" value="posterous_email,posterous_password,post_type" />
+               <input type="submit" value="<?php _e('Save Changes') ?>" />
+            </td>
+         </tr>
+      </table>
+      </form>
+      <?php
+   }
+   
+}
+
+function posterous_email(){
+   echo get_option('posterous_email');
+}
+
+
+function posterous_password(){
+   echo get_option('posterous_password');
+}
 
 
 function email_posterous($post_ID)  {
-    global $userdata;
-    get_currentuserinfo();
-      
-    $post = get_post($post_ID);
-    $recipient = 'posterous@posterous.com';
-    $subject = $post->post_title;
-    $body = '<a href="'.get_permalink($post_ID).'">'.$post->post_title.'</a>';
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    $headers .= "From: " . $userdata->user_email . "\n" . "Return-Path: " . $userdata->user_email . "\n" . "Reply-To: " . $userdata->user_email . "\n";
-    
-    if($post->post_type == 'post'){
-    	mail($recipient, $subject, $body, $headers);
-    }
-    return $post_ID;
+   if(get_option('posterous_email')!='' && get_option('posterous_password')!=''){
+      global $userdata;
+      get_currentuserinfo();
+
+      $post = get_post($post_ID);
+      $title = urlencode($post->post_title);
+      if(get_option('post_type')=="2"){
+         $body = urlencode($post->post_content);
+      }else{
+         $body = urlencode('<a href="'.get_permalink($post_ID).'">'.$post->post_title.'</a>');
+      }
+
+
+      $ch = curl_init(); 
+      curl_setopt($ch, CURLOPT_URL, 'http://posterous.com/api/newpost?title='.$title.'&body='.$body); 
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+      curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC); 
+      curl_setopt($ch, CURLOPT_USERPWD, "".get_option('posterous_email').":".get_option('posterous_password')."") ;
+
+      $data = curl_exec($ch); 
+      curl_close($ch);
+   }
 }
 
 add_action('draft_to_publish', 'email_posterous');
